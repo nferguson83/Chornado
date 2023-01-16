17aa7e9d7a0c7fcaa0fa550b8eeea1868c549f01
nferguson83 <106630905+nferguson83@users.noreply.github.com>
2023-01-16 15:13:14 +0000

    Correctly implement CSRF Protection

--- chornado_app/__init__.py
+++ chornado_app/__init__.py
@@ -1,21 +1,22 @@
 from os import environ, makedirs
 from flask import Flask
-from dotenv import load_dotenv
+from flask_wtf.csrf import CSRFProtect
+
+csrf = CSRFProtect()
 
 def create_app():
     '''Creates flask application session'''
 
     app = Flask(__name__, instance_relative_config=True)
-    # Load environment variables into OS, and remove .env for production
-    load_dotenv('../.env')
 
     dblogin = environ.get('DBLOGIN')
     dbpassword = environ.get('DBPASSWORD')
     dbdir = environ.get('DBDIR')
     dbwpassword = environ.get('DBWPASSWORD')
+    secret_key = environ.get('SECRET_KEY')
 
     app.config.from_mapping(
-        SECRET_KEY='dev', # Change to more secure key
+        SECRET_KEY=secret_key,
         SQLALCHEMY_DATABASE_URI=f'oracle+oracledb://{dblogin}:{dbpassword}@chornadodb_high/?config_dir={dbdir}&wallet_location={dbdir}&wallet_password={dbwpassword}&encoding=UTF-8&nencoding=UTF-8',
         SQLALCHEMY_TRACK_MODIFICATIONS=False
     )
@@ -25,6 +26,8 @@ def create_app():
     except OSError:
         pass
 
+    csrf.init_app(app)
+    
     from chornado_app.sql_models import db
     db.init_app(app)
 
